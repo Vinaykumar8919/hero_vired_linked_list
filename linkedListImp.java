dfc60645fda13d6a7cf2ab7732af542de71556f3
Vinaykumar8919 <vinaykumar891940@gmaol.com>
2023-07-11 11:49:08 +0530

    first commit

---
+++ linkedListImp.java
@@ -0,0 +1,18 @@
+class Node{
+    public Object item;
+    public Node next;
+}
+
+
+public class linkedListImp {
+    private int size =  0;
+    private Node head;
+    public linkedListImp() {
+        this.size = 0;
+        this.head=null;
+    }
+    public void insertingNode() {
+        
+    }
+
+}
